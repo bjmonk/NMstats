b865611bc0013e7a0414a244f2d3b71db454f33b
bjmonk <135523651+bjmonk@users.noreply.github.com>
2023-09-07 13:25:19 -0400

    Error checking

--- R/combs.R
+++ R/combs.R
@@ -9,11 +9,17 @@
 #' @param r The number of items to choose from `n`.
 #'
 #' @return
-#' An integer value representing the number of combinations of `r` items chosen from `n`.
+#' An integer value representing the number of combinations of `r` items chosen from `n`. If invalid input values are given, an error message is returned.
 #'
 #' @export
 
 combs <- function(n, r) {
+
+# Check if n and r are non-negative integers
+  if (!is.numeric(n) || !is.numeric(r) || n != as.integer(n) || r != as.integer(r) || n < 0 || r < 0) {
+    stop("Both n and r must be non-negative integers")
+  }
+
   if (r > n) {
     stop("r should not be greater than n")
   }
