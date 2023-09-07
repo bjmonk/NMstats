f0a0f8ca16d1845dde2b9b74126fa84c43d9ce99
bjmonk <135523651+bjmonk@users.noreply.github.com>
2023-09-07 14:32:55 -0400

    Rewrite to make more robust

--- R/rel_hist.R
+++ R/rel_hist.R
@@ -1,46 +1,79 @@
-#' Relative Histogram
+#' Relative Frequency Histogram
 #'
 #'
-#' The function `rel_hist` computes a relative histogram of given data values.
+#' The function `rel_hist` computes a relative frequency histogram of given data values.
 
-#' @usage rel_hist(x, ...)
-#' @param x   a vector of values for the relative histogram
-#' @param ... further arguments and graphical parameters available in the `hist` function.
+#' @usage rel_hist(data, bins = 30, col = "steelblue", xlab = "Data", ylab = "Relative Frequencies", main = "Relative Frequency Histogram", ybreaks = NULL)
+
+#' @param data A numeric vector containing the data to be plotted.
+#' @param bins The number of bins to use for the histogram. The default is 30.
+#' @param col The fill color for the histogram bars.
+#' @param xlab The label for the x-axis.
+#' @param ylab The label for the y-axis.
+#' @param main The main title for the plot.
+#' @param ybreaks A numeric vector specifying where to place tick marks on the y-axis. If NULL, tick marks are automatically calculated.
 #'
 #' @importFrom graphics hist
 #'
 #' @export
 
-rel_hist <- function(x,
-                          main = "Relative Histogram",
-                          ylab = "Relative Frequencies",
-                          ybreaks = NULL,
-                          ...) {
-  # Create a histogram without plotting it
-  h <- hist(x, plot = FALSE, ...)
+# Define the function rel_hist
+rel_hist <-
+  function(data,
+           bins = 30,
+           col = "steelblue",
+           xlab = "Data",
+           ylab = "Relative Frequency",
+           main = "Relative Frequency Histogram",
+           ybreaks = NULL) {
 
-  # Convert counts to relative frequencies
-  h$counts <- h$counts / sum(h$counts)
+    # Compute the histogram data without plotting
+    hist_data <- hist(data, breaks = bins, plot = FALSE)
 
-  # Calculate default y-axis breaks if not provided
-  if (is.null(ybreaks)) {
-    ybreaks <- seq(0, max(h$counts), length.out = 6)
+    # Calculate relative frequencies
+    rel_freqs <- hist_data$counts / sum(hist_data$counts)
+
+    # If ybreaks is not provided, calculate it based on max frequency. This ensures round numbers on y-axis.
+    if (is.null(ybreaks)) {
+      maxcount = max(rel_freqs)
+      mag = -floor(log10(maxcount))
+      k = 1
+      if (maxcount < 0.4)
+        k = 2
+      if (maxcount < 0.2)
+        k = 4
+      mx = ceiling(k * maxcount * (10 ^ mag))
+      ybreaks = c(0:mx) / (k * (10 ^ mag))
+    }
+
+    # Initialize the plot
+    xlim_range <- range(hist_data$breaks)
+    ylim_range <- c(0, max(ybreaks))
+    plot(
+      0,
+      0,
+      type = "n",
+      xlim = xlim_range,
+      ylim = ylim_range,
+      xlab = xlab,
+      ylab = ylab,
+      main = main,
+      yaxt = 'n'
+    )
+    axis(2, at = ybreaks, labels = ybreaks)
+
+    # Add bars with relative frequencies
+    bar_positions <- hist_data$mids
+    bar_heights <- rel_freqs
+    bar_widths <- diff(hist_data$breaks)[1]  # Assume equal bin widths
+
+    for (i in seq_along(bar_positions)) {
+      rect(
+        bar_positions[i] - bar_widths / 2,
+        0,
+        bar_positions[i] + bar_widths / 2,
+        bar_heights[i],
+        col = col
+      )
+    }
   }
-
-  # Filter out hist-specific arguments before calling plot
-  plot_args <- list(...)
-  unwanted_args <- c("breaks", "freq", "probability", "include.lowest", "right")
-  plot_args <- plot_args[!(names(plot_args) %in% unwanted_args)]
-
-  # Plot the relative histogram without the y-axis
-  do.call(plot, c(list(h, main = main, ylab = ylab, yaxt = 'n'), plot_args))
-
-  # Add y-axis with specified tick marks
-  axis(side = 2, at = ybreaks, labels = round(ybreaks, 3))
-
-  # Return the breaks, counts, and mids as a named list for further use
-  result <- list(breaks = h$breaks, counts = h$counts, mids = h$mids)
-
-  return(invisible(result))
-
-}
